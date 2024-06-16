adff64fa8ad47da75a87cb3e26e705539e19541f
Dzokara <95583354+Dzokara@users.noreply.github.com>
2024-06-16 21:58:01 +0200

    Add project files.

---
+++ AspProjekat.API/Core/ContainerExtensions.cs
@@ -0,0 +1,59 @@
+﻿using AspProjekat.Application;
+using AspProjekat.Implementation.Logging.UseCases;
+using AspProjekat.Implementation;
+using System.IdentityModel.Tokens.Jwt;
+using AspProjekat.Implementation.Validators;
+using AspProjekat.Application.UseCases.Commands.Users;
+using AspProjekat.Implementation.UseCases.Commands.Users;
+
+namespace AspProjekat.API.Core
+{
+    public static class ContainerExtensions
+    {
+        public static void AddUseCases(this IServiceCollection services)
+        {
+            //services.AddTransient<CreateCategoryDtoValidator>();
+            //services.AddTransient<UpdateUserAccessDtoValidator>();
+            //services.AddTransient<CreateImagePostDtoValidator>();
+            //services.AddTransient<ICreateCategoryCommand, EfCreateCategoryCommand>();
+            //services.AddTransient<IUpdateUseAccessCommand, EfUpdateUserAccessCommand>();
+            //services.AddTransient<ICreateImagePostCommand, EfCreateImagePostCommand>();
+            //services.AddTransient<UpdateCategoryValidator>();
+            services.AddTransient<UseCaseHandler>();
+            services.AddTransient<IRegisterUserCommand, EfRegisterUserCommand>();
+            services.AddTransient<IUseCaseLogger, SPUseCaseLogger>();
+            services.AddTransient<RegisterUserDtoValidator>();
+            //services.AddTransient<IGetUsersQuery, EfGetUsersQuery>();
+            //services.AddTransient<ILikePostCommand, EfLikePostCommand>();
+        }
+
+        public static Guid? GetTokenId(this HttpRequest request)
+        {
+            if (request == null || !request.Headers.ContainsKey("Authorization"))
+            {
+                return null;
+            }
+
+            string authHeader = request.Headers["Authorization"].ToString();
+
+            if (authHeader.Split("Bearer ").Length != 2)
+            {
+                return null;
+            }
+
+            string token = authHeader.Split("Bearer ")[1];
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var tokenObj = handler.ReadJwtToken(token);
+
+            var claims = tokenObj.Claims;
+
+            var claim = claims.First(x => x.Type == "jti").Value;
+
+            var tokenGuid = Guid.Parse(claim);
+
+            return tokenGuid;
+        }
+    }
+}
---
+++ AspProjekat.Domain/Job.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspProjekat.Domain
+{
+    public class Job : Entity
+    {
+        public int RegionId { get; set; }
+        public int TypeId { get; set; }
+        public int RemoteId { get; set; }
+        public int CompanyId { get; set; }
+        public int PositionId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public virtual Region Region { get; set; }
+
+        public virtual Type Type { get; set; }
+
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+        public virtual Remote Remote{ get; set; }
+
+        public virtual Company Company { get; set; }
+
+        public virtual Position Position { get; set; } 
+
+        public virtual ICollection<Benefit> Benefits { get; set; } = new HashSet<Benefit>();
+
+        public virtual ICollection<Technology> Technology { get; set; } = new HashSet<Technology>();
+
+        public virtual ICollection<User> SavedUsers {  get; set; }  = new HashSet<User>();  
+
+    }
+}
